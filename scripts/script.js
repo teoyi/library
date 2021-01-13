37750f3dbf39ac6091bb3782664518ff0d128258
teoyi <yipengteo@yahoo.com.sg>
2021-01-12 20:49:22 -0800

    Added constructor for books

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let myLibrary = [];
+
+function book(title, author, pages, status){
+    this.title = title; 
+    this.author = author;
+    this.pages = pages; 
+    this.read = status; // False would imply having not read the book yet
+    this.info = function() {
+        if (this.read === 'no') {
+            console.log(this.title + ' by ' + this.author + ', ' + this.pages + 'pages' + ', ' + 'Not Read');
+        } else if (this.read === 'yes'){
+            console.log(this.title + ' by ' + this.author + ', ' + this.pages + 'pages' + ', ' + 'Read');
+        };
+    };
+};
+
+const hobbit = new book('The Hobbit', 'J.R.R Tolkien', 295, 'yes');
+console.log(hobbit.info());
+
+function addBook(){
+
+};
