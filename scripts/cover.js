51d06bd6a08663e5a330c9629d2e0288624e09f0
teoyi <yipengteo@yahoo.com.sg>
2021-01-16 21:53:26 -0800

    Completed cover page and button for redirecring to main page

---
+++ scripts/cover.js
@@ -0,0 +1,7 @@
+const entry = document.querySelector('#entry');
+
+entry.addEventListener('click', function(){
+    console.log('RE-DIRECTING TO MAIN PAGE...');
+    window.location = 'index.html';
+});
+
