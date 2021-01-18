894b440626b361af9bda5f3055afd87d5ab21f53
teoyi <yipengteo@yahoo.com.sg>
2021-01-17 21:24:50 -0800

    Added burger and mobile screen transition

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,31 @@ function addBook(bookObj){
 
 addBook(hobbit);
 console.log(myLibrary);
+
+
+// Navigation tings 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+
+    burger.addEventListener('click', ()=>{
+
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links 
+        navLinks.forEach((link, index)=>{
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            };
+        });
+
+        // Burger Cross
+        burger.classList.toggle('toggle');
+    });
+};
+navSlide();
