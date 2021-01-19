a352d15b6cbfba730c133eb2ea3c4bc8c7187071
teoyi <yipengteo@yahoo.com.sg>
2021-01-18 23:32:11 -0800

    Added table append function as well as css styling to table

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,48 @@
 let myLibrary = [];
 
+// Parent that contains information about the object
 function book(title, author, pages, status){
     this.title = title; 
     this.author = author;
     this.pages = pages; 
-    this.read = status; // False would imply having not read the book yet
-    this.info = function() {
-        if (this.read === 'no') {
-            console.log(this.title + ' by ' + this.author + ', ' + this.pages + 'pages' + ', ' + 'Not Read');
-        } else if (this.read === 'yes'){
-            console.log(this.title + ' by ' + this.author + ', ' + this.pages + 'pages' + ', ' + 'Read');
-        };
-    };
+    this.read = status; // Indication of yes or no will relate to read or not read respectively
 };
 
-const hobbit = new book('The Hobbit', 'J.R.R Tolkien', '295', 'yes');
-const got = new book('The Game of Thrones', 'George R.R Martin', '694', 'no');
-console.log(hobbit.info());
-console.log(got.info())
-
+// Function to create book object and append to array 
 function addBook(bookObj){
     myLibrary.push(bookObj);
 };
 
+// Function to iterate and print information about books in library 
+function iter(lst){
+    lst.forEach(book => console.log(book));
+};
+
+// Function to update the table
+const tableContent = document.querySelector('.book-list')
+function update(){
+    tableContent.innerHTML = "";
+    myLibrary.forEach((book) => {
+        const htmlBook = `
+            <tr>
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td><button class="status-button">${book.status}</button></td>
+                <td><button class="delete">x</button></td>
+            </tr>
+            `;
+        tableContent.insertAdjacentHTML("afterbegin", htmlBook);
+    });
+    console.log('updated!');
+};
+
+
+
+const hobbit = new book('The Hobbit', 'J.R.R Tolkien', '295', 'yes');
+const got = new book('The Game of Thrones', 'George R.R Martin', '694', 'no');
+
 addBook(hobbit);
 addBook(got);
-console.log(myLibrary[0]);
+iter(myLibrary);
+update();
